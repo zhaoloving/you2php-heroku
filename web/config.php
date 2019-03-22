856c2bef5a4988ee9cebadf2bc7e7cccc2be6754
sweetpotato <justsweetpotato@gmail.com>
2019-03-22 19:06:32 +0800

    修复 PHP 版本错误, 去除密码, 更换 API key

--- web/config.php
+++ web/config.php
@@ -1,10 +1,10 @@
 <?php
 $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyC2mp6eD1vucTgiH5lbkr5WcE8mZuHe6Fo');
+define('APIKEY', 'AIzaSyBmYQPhWRuVcFG8XNO-WDBFLZpZtIkKlbY');
 define('GJ_CODE', $gl);
-define('SITE_NAME', 'JJ5CM');
-define('TITLENAME', 'JJ5CM');
+define('SITE_NAME', 'YoutubeMirror');
+define('TITLENAME', 'YoutubeMirror');
 define('EN2DEKEY', 'AnQUP1XDEF87ewemhuYG35e67CtzDSh4');
-define('EMAIL', 'justsweetpotato@gmail.com');
+define('EMAIL', 'None@gmail.com');
 ?>
