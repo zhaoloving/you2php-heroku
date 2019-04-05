dfc3f110f95e1ad1a1a3c3a3757aea1d7bddf856
justsweetpotato <43466248+justsweetpotato@users.noreply.github.com>
2019-04-05 22:28:43 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -3,8 +3,8 @@ $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyBmYQPhWRuVcFG8XNO-WDBFLZpZtIkKlbY');
 define('GJ_CODE', $gl);
-define('SITE_NAME', 'YouTubeMirror');
-define('TITLENAME', 'YouTubeMirror');
+define('SITE_NAME', 'Hello World!');
+define('TITLENAME', 'Hello World!');
 define('EN2DEKEY', 'AnQUP1XDEF87ewemhuYG35e67CtzDSh4');
 define('EMAIL', 'None@gmail.com');
 ?>
